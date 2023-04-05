b47cc8ada339a9a96b921444145e36506014f5f0
ASHISH SONI <ashishsoni2002@gmail.com>
2023-04-05 15:37:12 +0530

    frontend-fix

--- frontend/src/pages/content/Home.js
+++ frontend/src/pages/content/Home.js
@@ -2,10 +2,12 @@ import React, { useState } from "react";
 import "./Content.css";
 // import Project from "./Project";
 import Buttonz from "./components/Button.js";
+import Button_2 from "./components/Button_2.js";
 import ReactSlider from "./components/ReactSlider.js";
 
 const Home = () => {
   const [activeState, setActiveState] = useState("Data Analysis");
+  const [activeState2, setActiveState2] = useState("Maps Taxonomic");
   return (
     <>
       <div className="on-home-bg">
@@ -25,53 +27,117 @@ const Home = () => {
         </div>
       </div>
 
-      <div className="row white-area">
-        <div className="column">
-          {activeState === "Data Analysis" && (
-            <div className="Show data">
-              <h2 className="heading-footer ">
-                {/* <img className="footer-logo" src={logo} alt="" /> */}
-                <h2 className="Center">{activeState}</h2>
-              </h2>
-              <p>
-                Advanced data analysis of sales, <br /> margin, and cost to the
-                company <br />
-                with comprehensive month wise <br /> and year wise visualization
-                charts.
-              </p>
-            </div>
-          )}
+      <div className="section1">
+        <div className="row white-area">
+          <div className="column">
+            {activeState === "Data Analysis" && (
+              <div className="Show data">
+                <h2 className="heading-footer ">
+                  {/* <img className="footer-logo" src={logo} alt="" /> */}
+                  <h2 className="Center">{activeState}</h2>
+                </h2>
+                <p>
+                  Advanced data analysis of sales, <br /> margin, and cost to
+                  the company <br />
+                  with comprehensive month wise <br /> and year wise
+                  visualization charts.
+                </p>
+              </div>
+            )}
 
-          {activeState === "Popularity" && (
-            <div className="Show data">
-              <h2 className="heading-footer ">
-                {/* <img className="footer-logo" src={logo} alt="" /> */}
-                <h2 className="Center">{activeState}</h2>
-              </h2>
-              <p>
-                Comprehensive insights into products <br /> and brands
-                performance based on raw data,
-                <br /> using advanced animated charts
-              </p>
-            </div>
-          )}
+            {activeState === "Popularity" && (
+              <div className="Show data">
+                <h2 className="heading-footer ">
+                  {/* <img className="footer-logo" src={logo} alt="" /> */}
+                  <h2 className="Center">{activeState}</h2>
+                </h2>
+                <p>
+                  Comprehensive insights into products <br /> and brands
+                  performance based on raw data,
+                  <br /> using advanced animated charts
+                </p>
+              </div>
+            )}
 
-          {activeState === "Tree Map" && (
-            <div className="Show data">
-              <h2 className="heading-footer ">
-                {/* <img className="footer-logo" src={logo} alt="" /> */}
-                <h2 className="Center">{activeState}</h2>
-              </h2>
-              <p>
-                Detailed view of distribution based <br /> on popularity, sales,
-                and margin <br /> with our informative and visually <br />{" "}
-                appealing tree map.{" "}
-              </p>
-            </div>
-          )}
+            {activeState === "Tree Map" && (
+              <div className="Show data">
+                <h2 className="heading-footer ">
+                  {/* <img className="footer-logo" src={logo} alt="" /> */}
+                  <h2 className="Center">{activeState}</h2>
+                </h2>
+                <p>
+                  Detailed view of distribution based <br /> on popularity,
+                  sales, and margin <br /> with our informative and visually{" "}
+                  <br /> appealing tree map.{" "}
+                </p>
+              </div>
+            )}
+          </div>
+          <div className="column">
+            <Buttonz setActiveState={setActiveState} />
+          </div>
         </div>
-        <div className="column">
-          <Buttonz setActiveState={setActiveState} />
+      </div>
+      {/*  */}
+      {/*  */}
+      {/*  */}
+      {/*  */}
+
+      {/*
+       */}
+      {/*  */}
+      {/* new section  */}
+      {/*  */}
+      <div className="section2">
+        <div className="row white-area">
+          <div className="column">
+            <Button_2 setActiveState={setActiveState2} />
+          </div>
+
+          <div className="column">
+            {activeState2 === "Maps Taxonomic" && (
+              <div className="Show data">
+                <h2 className="heading-footer ">
+                  {/* <img className="footer-logo" src={logo} alt="" /> */}
+                  <h2 className="Center">{activeState2}</h2>
+                </h2>
+                <p>
+                  Advanced data analysis of sales, <br /> margin, and cost to
+                  the company <br />
+                  with comprehensive month wise <br /> and year wise
+                  visualization charts.
+                </p>
+              </div>
+            )}
+
+            {activeState2 === "Taxonomic" && (
+              <div className="Show data">
+                <h2 className="heading-footer ">
+                  {/* <img className="footer-logo" src={logo} alt="" /> */}
+                  <h2 className="Center">{activeState2}</h2>
+                </h2>
+                <p>
+                  Comprehensive insights into products <br /> and brands
+                  performance based on raw data,
+                  <br /> using advanced animated charts
+                </p>
+              </div>
+            )}
+
+            {activeState2 === "Data Taxonomic" && (
+              <div className="Show data">
+                <h2 className="heading-footer ">
+                  {/* <img className="footer-logo" src={logo} alt="" /> */}
+                  <h2 className="Center">{activeState2}</h2>
+                </h2>
+                <p>
+                  Detailed view of distribution based <br /> on popularity,
+                  sales, and margin <br /> with our informative and visually{" "}
+                  <br /> appealing tree map.{" "}
+                </p>
+              </div>
+            )}
+          </div>
         </div>
       </div>
 
